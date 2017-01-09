9a2e39ccb68408064cd9305fec95799844edace0
Mick van Duijn <m.vanduijn@zefiros.eu>
2017-01-09 10:42:34 +0100

    Renamed tests

--- test/objectTest.cpp
+++ test/objectTest.cpp
@@ -1,6 +1,6 @@
 #include "testClasses.h"
 
 #define SERIALISATION_ARRAY_TEST( type )                                                                               \
-SERIALISATION_TEST( Object_Zebra, Nested, NestedObject< type >, type, GenerateZebraValue<type>(), GetRandom<type>() );
+SERIALISATION_TEST( Object, Nested_Zebra, NestedObject< type >, type, GenerateZebraValue<type>(), GetRandom<type>() );
 
 SERIALISATION_ALL_TYPES( SERIALISATION_ARRAY_TEST );
