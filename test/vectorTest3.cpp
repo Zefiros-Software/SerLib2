5c14456812ce90d0d287b1f76c94a08b578dbe42
Mick van Duijn <m.vanduijn@zefiros.eu>
2017-01-16 17:48:48 +0100

    Split vectorTest file to fit compiler memory with ICC

---
+++ test/vectorTest3.cpp
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2017 Zefiros Software.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include "testClasses.h"
+
+typedef TestClassArray< bool, 0x1 > PackedBoolVector;
+SERIALISATION_TEST( Container, BoolPackedVector, PackedBoolVector, bool, MakeSeed<bool>( 424142 ), 424142 )
+
+typedef TestClassArrayOfSize< bool, 0x1, 1024 > PackedBoolVectorOfSize;
+SERIALISATION_TEST( Container, BoolPackedVectorOfSize, PackedBoolVectorOfSize, bool, MakeSeed<bool>( 4241 ), 4241 );
