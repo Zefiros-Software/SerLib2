b2e2078de13962e0abcdc9194eff94a05945a34c
PaulVisscher <paul.pev@hotmail.com>
2017-01-17 20:48:11 +0100

    Fix appveyor builds

--- script/appveyor-build.bat
+++ script/appveyor-build.bat
@@ -7,16 +7,18 @@ if "%TYPE%" == "zpm" (
 
     msbuild zpm/SerLib-ZPM.sln || exit /b 1
 
-    test\bin\x86\serialisation-zpm-test.exe || exit /b 1
+    cd bin\x86\
+    serialisation-zpm-test.exe || exit /b 1
 ) else (
     zpm install-package --allow-install --allow-module || exit /b 1
     zpm %VSTUD% --allow-install || exit /b 1
 
     msbuild serialisation/SerLib.sln /property:Configuration=Release /property:Platform=%PLAT% || exit /b 1
 
+    cd bin\%ARCH%\
     if %TYPE% == "debug" (
-        bin\%ARCH%\serialisation-testd
+        serialisation-testd
     ) else (
-        bin\%ARCH%\serialisation-test
+        serialisation-test
     )
 )
